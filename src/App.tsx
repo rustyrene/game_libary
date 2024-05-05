1dcefb16f4a511643c86820da7c420861e7a09f1
René Delion <rene.delion.buisness@protonmail.com>
2024-05-05 16:46:31 +0200

    Install Chakra Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button, ButtonGroup } from "@chakra-ui/react"
+
 function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <Button colorScheme='blue'>Test</Button>
+    </>
   )
 }
 
